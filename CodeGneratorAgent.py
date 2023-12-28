3bea64046faf40c3432ac3ba54269a075200a8c5
xf4c70r <arvindsudarshan18@gmail.com>
2023-12-28 11:10:11 -0800

    Rewrote the sprite generator and code generator using classes and added a main.py file to handle execution of the agents

---
+++ CodeGneratorAgent.py
@@ -0,0 +1,35 @@
+import os
+import json
+
+api = os.environ.get('OPENAI_API_KEY')
+
+class CodeGeneratorAgent:
+
+    def __init__(self, client, sys_prompt:str) -> None:
+        self.client = client
+        self.sys_prompt = sys_prompt
+    
+    def codeGen(self, user_prompt:str) -> dict:
+        # Generate Code
+        codeResponse = self.client.chat.completions.create(
+            model="gpt-4-1106-preview",
+            response_format={ "type": "json_object" },  
+            messages=[
+                {"role": "system", "content": self.sys_prompt},
+                {"role": "user", "content": user_prompt},
+            ]
+        )
+
+        # Extracted code from the response
+        formattedCode = codeResponse.choices[0].message.content
+        return json.loads(formattedCode)
+    
+    def createGameFile(self, formattedCodeJSON:dict, parent_dir:str):
+        # File path for game.py in the parent directory
+        file_path = os.path.join(parent_dir, "game.py")
+
+        # Save the code to game.py in the parent folder
+        with open(file_path, "w") as file:
+            file.write(formattedCodeJSON['Code'])
+
+        print(f"Code saved to {file_path}")
--- SpriteGeneratorAgent.py
+++ SpriteGeneratorAgent.py
@@ -2,82 +2,72 @@ import os
 import json
 import shutil
 import requests
-from openai import OpenAI
 
-api_key = os.environ.get('OPENAI_API_KEY')
+class SpriteGeneratorAgent:
 
-client = OpenAI(api_key= api_key)
+    def __init__(self, client, sys_prompt:str) -> None:
+        self.client = client
+        self.sys_prompt = sys_prompt
 
-# System prompts is used to set the stage or context for the conversation, and it can influence how the model responds to user inputs.
-sys_prompt = "You are my game developer. Help me create good games. We are going to generate 2D games using a python library called PyGame. These will be your task: 1. Identify all the sprites/assets required for the game.  2. Give me a response back in JSON format. The JSON object should have exactly the same amount of keys as the number of sprites identified. The Key-Value pair in the JSON file should be such that the key is the name of the sprite and the value is the description of the sprite/asset such that it would help DALL-E generate images for the required sprites or images. 4. Also generate a good name for the game. Do not use any special charecters in the name of the game. The key for the name of the game in the JSON response should strictly be 'name'. Example of the JSON Object: {\"name\": \"Streer Racer\",\"car\": \"A green colored car\", \"obstacles\": \"Brown boulders\", \"background\": \"A road with yellow colored strips to divide it into two different lanes\"}"
+    def spriteDescGen(self, user_prompt:str) -> dict:
+        spriteResponse = self.client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        # max_tokens= 100, 
+        response_format={ "type": "json_object" },  
+        messages=[
+            {"role": "system", "content": self.sys_prompt},
+            {"role": "user", "content": user_prompt},
+        ]
+    )
+        # Extract the required content from the response and convert it to JSON Object
+        imageDescription = spriteResponse.choices[0].message.content
+        return json.loads(imageDescription)
+    
+    def createFolder(self, imageDescriptionJSON:dict):
+        # Create a folder with the name of the game
+        parentFolderName = imageDescriptionJSON.pop('name')
+        parentFolderPath = os.path.join(os.getcwd(), parentFolderName)
 
-user_prompt1 = "Generate me a racing game where I am a car trying to avoid traffic cones on the road. The car only moves left or right. It automatically keeps moving forward. The Cones can spawn at any place randomly only they should not overlap with the car. "
+        # Check if the folder alredy exists
+        if os.path.exists(parentFolderPath):
+            shutil.rmtree(parentFolderPath)
+        else:
+            # Create the game folder if it does not exist
+            os.makedirs(parentFolderPath)
 
-user_prompt2 = "Generate me a side scroling game with a hero who has a sword and uses magic bullets. He cannot use the magic bullets once his MP runs out and MP is reduced with each magic bullet he fires and he also has an HP which is reduced each time an enemy hits the hero. The enemies are orges."
+        # Create Assets folder
+        imageFolderPath = os.path.join(parentFolderPath, 'assets')
 
-user_prompt3 = "Generate me a pygame game where I as a spaceship  shoot aliens and asteroids are obstacles that I avoid by moving either left or right the spaceship keeps on moving straight and If the space ship colides with the asteroid it is game over. The score is the number of aliens I succesfully shoot. For shooting use space bar. Give me the whole implimentation. Also see to it that the sprites are sized appropriately"
+        # Check if the assets folder exists
+        if os.path.exists(imageFolderPath):
+            shutil.rmtree(imageFolderPath)
+        else:
+            # Create the assets folder if it does not exist
+            os.makedirs(imageFolderPath)
 
-# Get the description of the sprites to be generated
-spriteResponse = client.chat.completions.create(
-    model="gpt-4-1106-preview",
-    # max_tokens= 100, 
-    response_format={ "type": "json_object" },  
-    messages=[
-        {"role": "system", "content": sys_prompt},
-        {"role": "user", "content": user_prompt3},
-    ]
-)
+        # Get the names of the assets
+        assetNames = list(imageDescriptionJSON.keys())
+        return parentFolderPath, imageFolderPath, assetNames
+    
+    def downloadAssets(self, imageDescriptionJSON:dict, imageFolderPath:str):
+        # Iterate over the JSON Object to generate Images according to the description
+        for key,value in imageDescriptionJSON.items():
 
-# Extract the required content from the response and convert it to JSON Object
-imageDescription = spriteResponse.choices[0].message.content
-imageDescriptionJSON = json.loads(imageDescription)
+            # Generate Image for each key value pair in the JSON Object
+            response = self.client.images.generate(
+            model="dall-e-3",
+            prompt= value,
+            size="1024x1024",
+            quality="standard",
+            n=1,) 
 
-# Create a folder with the name of the game
-parentFolderName = imageDescriptionJSON.pop('name')
-parentFolderPath = os.path.join(os.getcwd(), parentFolderName)
-
-# Check if the folder alredy exists
-if os.path.exists(parentFolderPath):
-    shutil.rmtree(parentFolderPath)
-else:
-    # Create the game folder if it does not exist
-    os.makedirs(parentFolderPath)
-
-print("Parent folder path: ", parentFolderPath)
-
-# Create Assets folder
-imageFolderPath = os.path.join(parentFolderPath, 'assets')
-
-# Check if the assets folder exists
-if os.path.exists(imageFolderPath):
-    shutil.rmtree(imageFolderPath)
-else:
-    # Create the assets folder if it does not exist
-    os.makedirs(imageFolderPath)
-
-
-# Get the names of the assets
-assetNames = list(imageDescriptionJSON.keys())
-print(assetNames)
-
-# Iterate over the JSON Object to generate Images according to the description
-for key,value in imageDescriptionJSON.items():
-
-    # Generate Image for each key value pair in the JSON Object
-    response = client.images.generate(
-    model="dall-e-3",
-    prompt= value,
-    size="1024x1024",
-    quality="standard",
-    n=1,) 
-
-    # Download the Image and store it in the assets folder
-    image_url = response.data[0].url
-    response = requests.get(image_url) 
-    if response.status_code == 200:
-        image_path = os.path.join(imageFolderPath, f'{key}.png')
-        with open(image_path, 'wb') as file:
-            file.write(response.content)
-        print(f"Image {key} downloaded and saved as '{image_path}'")
-    else:
-        print(f"Failed to download image {key}")
+            # Download the Image and store it in the assets folder
+            image_url = response.data[0].url
+            response = requests.get(image_url) 
+            if response.status_code == 200:
+                image_path = os.path.join(imageFolderPath, f'{key}.png')
+                with open(image_path, 'wb') as file:
+                    file.write(response.content)
+                print(f"Image {key} downloaded and saved as '{image_path}'")
+            else:
+                print(f"Failed to download image {key}")
